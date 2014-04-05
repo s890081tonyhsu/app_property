a072de790bad343dfb5c8285a737ba9362afbb11
OtomeSound <s890081tonyhsu@gmail.com>
2014-04-05 18:11:54 +0800

    Add item structure and setup menu.
    1.	item_controller add new, not worked function.
    2.	Make sub_menu work.
    3.	Create new, show page and modify view page.
    4.	Setup some routes to work for item show links.

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -2,4 +2,15 @@ class ItemController < ApplicationController
   def view_item
     @itemAll = Item.all
   end
+  def new_item
+    @itemNew = Item.new
+  end
+  def create_item
+    @itemCreate = Item.new(params[:item])
+	@itemCreate.save
+  end
+  def show_item
+    @itemId = params[:id]
+    @itemShow = Item.find(@itemId)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ AppProperty::Application.routes.draw do
   root to: 'visitors#new'
   get "item" => "item#view_item"
   get "item/view" => "item#view_item"
+  get "item/new" => "item#new_item"
+  get "item/modify/:id" => "item#modify"
+  get "item/del/:id" => "item#del"
+
+  post "item/create_item"
+  get "item/:id" => "item#show_item"
+  put "item/updating/:id" => "item#updating"
+  put "item/destroying/:id" => "item#destroying"
 end
