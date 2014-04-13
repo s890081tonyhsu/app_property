d7e866d2ab98de2768ce2861e70c594d83406718
OtomeSound <s890081tonyhsu@gmail.com>
2014-04-13 23:43:51 +0800

    Lend basic validation finished.

--- app/models/lend.rb
+++ app/models/lend.rb
@@ -1,6 +1,11 @@
 class Lend < ActiveRecord::Base
   # 欄位非空
-  validates_presence_of :LendName, :LendEmail, :ItemId, :ItemLendStatus, :PassTime, :DeadTime
-  # 相同物品名下不允許同名借閱者
-  validates_uniqueness_of :ItemId, :scope => :LendName
+  validates_presence_of :LendName, :LendEmail, :ItemId, :ItemLendStatus, :PassTime, :DeadTime, :message => "該欄位不能為空白"
+  # 姓名驗證
+  validates_length_of :LendName, :in => 1..32, :message => "名字限制在1~32字之內"
+  validates_uniqueness_of :ItemId, :scope => :LendName, :message => "你已經借閱過該物品"
+  # 電子郵件確認
+  # 借用物品確認
+  # 預定借用日期確認
+
 end
