574f7b454f5cb924491211b98880479067272db3
OtomeSound <s890081tonyhsu@gmail.com>
2014-04-19 01:38:50 +0800

    Put some example setting file.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 AppProperty::Application.initialize!
+
+# This is for server side using.
+# ENV['RAILS_ENV'] ||='production'
